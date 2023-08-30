777f70a61ca553b6801621b6de21dfb26e1852a5
Shubham <shubhamdhaundiyal789@gmail.com>
2023-08-30 09:44:19 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,62 +1,36 @@
-import './App.css';
-import React,{ useState } from 'react';
+import "./App.css";
+import React, { useState } from "react";
 function App() {
-  const [weight,setWeight]=useState(0);
-  const [height,setHeight]=useState(0);
-  const [bmi,setBmi]=useState('');
-  const [message,setMessage]=useState('');
-
-  //Logic
-let calcBMI=(e)=>{
-  e.preventDefault();
-  if(weight === 0 || height===0){
-    alert("Please enter a valid height and weight")
-  }
-  else{
-    let bmi=(weight/(height*height)*703);
-    setBmi(bmi.toFixed(1));
-
-    //LOgic for message
-    if(bmi<17.5){
-      setMessage("You are underweight")
-    }
-    else if(bmi>=25 && bmi<30){
-      setMessage("You are normal")
-    }
-    else {
-      setMessage("You are overweight")
-    }
-  }
-}
-
-let reload=()=>{
-  window.location.reload();
-}
-
   return (
-    <div className="App">
-      <div className='container' >
-      <h2>BMI Calculator</h2>
-      <form onSubmit={calcBMI}>
-        <div>
-          <label htmlFor="">Weight(lbs)</label>
-          <input type="text" placeholder='Enter Weight Value' value={weight} onChange={(e)=>setWeight(e.target.value)} />
-        </div>
-        <div>
-          <label htmlFor="">Height(in)</label>
-          <input type="text" placeholder='Enter Height Value' value={height} onChange={(e)=>setHeight(e.target.value)} />
-        </div>
-        <div>
-          <button className='btn' type='submit' >Submit</button>
-          <button className='btn btn-outline' type='submit' onClick={reload} >Reload</button>
-        </div>
-        <div className="center">
-          <h3>Your BMI is:{bmi}</h3>
-          <p>{message}</p>
-        </div>
-      </form>
+    <div className="container">
+      <nav>
+        <img src="images/logo.png" className="logo" alt="logo" />
+        <ul>
+          <li>
+            <a href="www.google.com">Travel Guide</a>
+          </li>
+          <li>
+            <a href="www.google.com">Famous Places</a>
+          </li>
+          <li>
+            <a href="www.google.com">Contact Us</a>
+          </li>
+        </ul>
+        <button className="btn" >
+          <img src="images/icon.png" alt="Aeroplane" />
+          Bookings
+        </button>
+      </nav>
+      <div className="content">
+        <h1>Beautiful <br/>Places To Explore </h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi voluptatum qui necessitatibus reiciendis quidem porro voluptas nulla minus molestias odio esse rerum repellendus, fugit doloribus recusandae vitae sit, laudantium nam?</p>
+        <form action="">
+          <input type="text" placeholder="Country Name" />
+          <button type="Submit" className="btn" >Search</button>
+        </form>
       </div>
     </div>
+
   );
 }
 
